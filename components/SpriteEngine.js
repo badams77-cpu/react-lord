7d527670daf9a06addba751787523f2c5f136afb
Barry Adams <barry.david.adams@gmail.com>
2020-06-20 02:55:16 +0100

    changeroom debug dispatch

--- components/SpriteEngine.js
+++ components/SpriteEngine.js
@@ -238,30 +238,30 @@ class SpriteEngine extends Component {
                 mySprite.direction='right'; mySprite.anim_counter=0; }
              mySprite.delay_counter=0; newDirection=true;
                if (i==0){
-                 let myroom = this.split_room(this.state_room);
+                 let myroom = this.split_room(this.state.room);
                  let newx = myroom.room_x-1;
                  if (newx<1){ newx=8;}
-                 onChangeRoom('room_'+myroom.room_y+'_'+newx, (this.state.window_width-this.state.tile_width)/this.state.tile_width, mySprite.y/this.state.tile_height, );
+                 this.props.onChangeRoom('room_'+myroom.room_y+'_'+newx, (this.state.window_width-this.state.tile_width)/this.state.tile_width, mySprite.y/this.state.tile_height, );
                }
              }
             if (mySprite.y<0){
               if (i!=0) { mySprite.dy = 2; mySprite.direction='down'; mySprite.anim_counter=0; }
               mySprite.delay_counter=0; newDirection=true;
                if (i==0){
-                 let myroom = this.split_room(this.state_room);
+                 let myroom = this.split_room(this.state.room);
                  let newy = myroom.room_y-1;
                  if (newy<1){ newy=8;}
-                 onChangeRoom('room_'+newy+'_'+myroom.room_x, mySprite.x/tile.width, (this.state.window_height-this.state_tile_height)/this.state_tile_height );
+                 this.props.onChangeRoom('room_'+newy+'_'+myroom.room_x, mySprite.x/tile.width, (this.state.window_height-this.state_tile_height)/this.state_tile_height );
                }
               }
             if (mySprite.x + this.state.tile_width> this.state.window_width ){
                 if (i!=0) {mySprite.dx = -2; mySprite.direction='left';
                  mySprite.anim_counter=0; mySprite.delay_counter=0; newDirection=true; }
                 if (i==0){
-                   let myroom = this.split_room(this.state_room);
+                   let myroom = this.split_room(this.state.room);
                    let newx = myroom.room_x+1;
                    if (newx>8){ newx=1;}
-                   onChangeRoom('room_'+myroom.room_y+'_'+newx, 0, mySprite.y/this.state.tile_height, );
+                   this.props.onChangeRoom('room_'+myroom.room_y+'_'+newx, 0, mySprite.y/this.state.tile_height, );
                 }
             }
             if (mySprite.y + this.state.tile_height > this.state.window_height){
@@ -270,10 +270,10 @@ class SpriteEngine extends Component {
                   mySprite.anim_counter=0; mySprite.delay_counter=0; newDirection=true;
                 }
                 if (i==0){
-                     let myroom = this.split_room(this.state_room);
+                     let myroom = this.split_room(this.state.room);
                      let newy = myroom.room_y+1;
                      if (newy>8){ newy=1;}
-                     onChangeRoom('room_'+newy+'_'+myroom.room_x, mySprite.x/tile.width, 0 );
+                     this.props.onChangeRoom('room_'+newy+'_'+myroom.room_x, mySprite.x/tile.width, 0 );
                 }
 
             }
